55344ec5ea4e4201def7b888e1a7f42c5a65f096
Andy Roberts <andy.roberts@macmillan.com>
2018-09-14 18:51:50 +0100

    add nice test reports

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,8 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "mandate"
 
+
+require 'minitest/reporters'
+
 require "minitest/autorun"
+Minitest::Reporters.use!(Minitest::Reporters::SpecReporter.new)
